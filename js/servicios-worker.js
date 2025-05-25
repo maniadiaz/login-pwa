2dba82b121cdad71ea600d1c31975808b5bc4527
maniadiaz <mania518alexis@gmail.com>
2025-05-24 23:47:59 -0700

    modificacion y restructura de archivos

--- js/servicios-worker.js
+++ js/servicios-worker.js
@@ -5,14 +5,33 @@ const FILES_TO_CACHE = [
   '/login-pwa/dashboard.html',
   '/login-pwa/css/styles.css',
   '/login-pwa/js/main.js',
+  '/login-pwa/assets/icons/icon-192.png',
+  '/login-pwa/assets/icons/icon-512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css'
 ];
 
 self.addEventListener('install',(e) =>{
   e.waitUntil(
-    caches.open(CACHE_NAME)
-    .then((caches) => caches.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache)=>{
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
